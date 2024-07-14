3b520d85626805e8f61a1d84ed4b2d268efe8972
Arucaden <110232305+Arucaden@users.noreply.github.com>
2024-07-14 20:48:02 +0700

    Revert "add blog template (nto workong)"
    This reverts commit c6ec93b9bc752eac442249d9f0e746a655697a94.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,11 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import { defineCollection, z } from 'astro:content';
+
 import tailwind from "@astrojs/tailwind";
 
-const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.string(),
-    description: z.string(),
-    author: z.string(),
-  }),
-});
-
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    mdx(),
-  ], 
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  })],
   vite: {
     css: {
       preprocessorOptions: {
@@ -27,8 +14,5 @@ export default defineConfig({
         }
       }
     }
-  },
-  collections: {
-    blog: blogCollection,
-  },
+  }
 });
