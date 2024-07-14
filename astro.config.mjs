c6ec93b9bc752eac442249d9f0e746a655697a94
daffamaulanasatria <daffamaulanasatria@gmail.com>
2024-07-14 13:26:54 +0000

    add blog template (nto workong)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from 'astro/config';
-
+import mdx from '@astrojs/mdx';
+import { defineCollection, z } from 'astro:content';
 import tailwind from "@astrojs/tailwind";
 
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.string(),
+    description: z.string(),
+    author: z.string(),
+  }),
+});
+
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  })],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+  ], 
   vite: {
     css: {
       preprocessorOptions: {
@@ -14,5 +27,8 @@ export default defineConfig({
         }
       }
     }
-  }
+  },
+  collections: {
+    blog: blogCollection,
+  },
 });
