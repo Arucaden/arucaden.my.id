fdb6f87ee4bca079080cb33f64f7d4b6fef50d3a
Daffa Maulana Satria <daffamaulanasatria@gmail.com>
2025-04-06 23:10:14 +0700

    Understandable svelte + astro logic

---
+++ src/components/NavIsland.svelte
@@ -0,0 +1,8 @@
+<!-- src/components/NavIsland.svelte -->
+<script>
+    let date = new Date().toLocaleDateString('en-GB');
+  </script>
+  
+  <span class="text-xs text-pink-400">
+    {date}
+  </span>
--- uno.config.ts
+++ uno.config.ts
@@ -1,13 +1,17 @@
 // uno.config.ts
-import { defineConfig, presetAttributify, presetIcons, presetWind4 } from 'unocss'
+import { defineConfig, presetAttributify, presetWebFonts, presetWind4 } from 'unocss'
 
 export default defineConfig({
   presets: [
     presetWind4(),
     presetAttributify(),
+    presetWebFonts({
+      fonts: {
+        sans: 'JetBrains Mono',
+      },
+    }),
   ],
   shortcuts: [
-    // shortcut class custom buat efek CRT
     ['crt-bg', 'bg-[#140a0d] relative overflow-hidden'],
     ['crt-lines', 'absolute inset-0 bg-repeat bg-[linear-gradient(rgba(255,255,255,0.05)_0px,rgba(77,21,21,0.1)_2px,rgba(0,0,0,0.2)_4px)] opacity-30 animate-scanline z-10 pointer-events-none'],
     ['crt-text', 'text-pink-400 text-shadow-[0_0_5px_#ff4d6d,0_0_10px_#ff4d6d,0_0_15px_#ff4d6d] animate-flicker'],
@@ -15,7 +19,11 @@ export default defineConfig({
   theme: {
     colors: {
       'crt-red': '#ff4d6d',
-      'crt-bg': '#140a0d'
+      'crt-bg': '#140a0d',
+      'main': '#E7576C',
+      'secondary': '#BB404D',
+      'white-text': '#F7E2E5',
+      'black-text': '#310C0C',
     }
   },
   safelist: ['animate-scanline', 'animate-flicker'],
